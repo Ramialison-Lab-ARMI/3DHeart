c8b9a21e275b16ade45a4a4da2c27cabc339a0e4
nmt <nathaliamtan@gmail.com>
2015-11-02 15:45:00 +1100

    Added script that allows rotation of the heart model.

---
+++ Rotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow))
+			transform.Rotate(Vector3.down, 500 * Time.deltaTime);
+		if(Input.GetKey(KeyCode.RightArrow))
+			transform.Rotate(Vector3.up, 500 * Time.deltaTime);
+	}
+}
